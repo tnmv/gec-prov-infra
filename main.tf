f67a75eeeebb46517c1be34a0f904e19a90bfc45
Vianney NOUTENIJEU <vianneynoutenijeu@gmail.com>
2023-09-14 00:07:24 +0100

    adding some variables

--- main.tf
+++ main.tf
@@ -143,14 +143,29 @@ resource "aws_vpc" "vpc_master" {
   }
 }
 
-#resource "aws_instance" "ec2-virtual-machine" {
-# ami                         = ami-12345
-# instance_type               = t2.micro
-# key_name                    = aws_key_pair.master-key.key_name
-# associate_public_ip_address = true
-# vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
-# subnet_id                   = aws_subnet.subnet.id
-# provisioner "local-exec" {
-#   command = "aws ec2 wait instance-status-ok --region us-east-1 --instance-ids ${self.id}"
-#  }
-#}
+variable "region1" {
+    type = "string"
+    default = "us-east-1"
+}
+variable "id_image" {
+    type = string
+    default = "ami-0261755bbcb8c4a84"
+}
+variable "id_vpc" {
+    type = string
+    default = "vpc-0dee5d356fa292024"
+}
+variable "id_subnet" {
+    type = string
+    default ="subnet-0a799f29f38d852a5"
+}
+variable "key_access" {
+    type = "string"
+}
+variable "key_secret" {
+    type = "string"
+}
+variable "instance_type" {
+    type = "string"
+    default = "t2.medium"
+}
---
+++ variables.auto.tfvars
@@ -0,0 +1,10 @@
+region1 = "us-west-1"
+id_image = ""
+id_vpc = ""
+id_subnet = ""
+key_access = ""
+key_secret = ""
+instance_type = ""
+
+
+### variables for traffic
